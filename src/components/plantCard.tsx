dfdc6ddb3644db01429124d66f3fc380c6961351
zlrkw11 <ray040406@gmail.com>
2024-08-31 18:01:03 +1200

    group hover

--- src/components/plantCard.tsx
+++ src/components/plantCard.tsx
@@ -1,4 +1,5 @@
 import { PlantType } from "@/models/plant";
+import InfoComponent from "./info";
 
 export type PlantCardProps = {
   plant: PlantType;
@@ -8,6 +9,9 @@ const PlantCard = ({ plant }: PlantCardProps) => {
   return (
     <div className="border-black border py-36 flex justify-center rounded-lg m-1">
       Plant Card
+      <div className="hidden group-hover:block">
+        <InfoComponent />
+      </div>
     </div>
   );
 };
