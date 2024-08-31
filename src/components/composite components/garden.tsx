4dabd20be2681fe7629d67c2ff782e4d9c2fd184
zlrkw11 <ray040406@gmail.com>
2024-08-31 20:25:57 +1200

    map

--- src/components/composite components/garden.tsx
+++ src/components/composite components/garden.tsx
@@ -1,25 +1,54 @@
 import PlantCard from "@/components/plantCard";
 import { PlantType } from "@/models/plant";
-
+import Plant from "@/components/plant";
 const plants: PlantType[] = [
-  { id: "1", name: "Rose", type: "Flower", stage: 2, water: true },
-  { id: "1", name: "Tulip", type: "Flower", stage: 1, water: false },
-  { id: "1", name: "Cactus", type: "Succulent", stage: 3, water: true },
-  { id: "1", name: "Fern", type: "Fern", stage: 2, water: false },
-  { id: "1", name: "Sunflower", type: "Flower", stage: 4, water: true },
-  { id: "1", name: "Bamboo", type: "Grass", stage: 2, water: true },
-  { id: "1", name: "Lavender", type: "Herb", stage: 1, water: false },
-  { id: "1", name: "Orchid", type: "Flower", stage: 3, water: true },
-  { id: "1", name: "Mint", type: "Herb", stage: 1, water: true },
+  { id: "1", name: "Rose", type: "Flower", stage: 2, lastWatered: new Date() },
+  { id: "1", name: "Tulip", type: "Flower", stage: 1, lastWatered: new Date() },
+  {
+    id: "1",
+    name: "Cactus",
+    type: "Succulent",
+    stage: 3,
+    lastWatered: new Date(),
+  },
+  { id: "1", name: "Fern", type: "Fern", stage: 2, lastWatered: new Date() },
+  {
+    id: "1",
+    name: "Sunflower",
+    type: "Flower",
+    stage: 4,
+    lastWatered: new Date(),
+  },
+  { id: "1", name: "Bamboo", type: "Grass", stage: 2, lastWatered: new Date() },
+  {
+    id: "1",
+    name: "Lavender",
+    type: "Herb",
+    stage: 1,
+    lastWatered: new Date(),
+  },
+  {
+    id: "1",
+    name: "Orchid",
+    type: "Flower",
+    stage: 3,
+    lastWatered: new Date(),
+  },
+  { id: "1", name: "Mint", type: "Herb", stage: 1, lastWatered: new Date() },
 ];
 
 const Garden = () => {
   return (
     <div className="px-16 mt-8">
       <div className="grid grid-cols-3">
-        {plants.map((plant, index) => (
-          <PlantCard key={index} plant={plant} />
-        ))}
+        {plants.map((plant, index) => {
+          return (
+            <div key={index}>
+              <Plant key={index} plant={plant} />
+              <PlantCard key={index} plant={plant} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/plant.tsx
+++ src/components/plant.tsx
@@ -4,6 +4,14 @@ export type PlantProps = {
   plant: PlantType;
 };
 
-const Plant = ({ plant }: PlantProps) => {};
+const Plant = ({ plant }: PlantProps) => {
+  return (
+    <div className="">
+      <div className="border border-black">insert plant image here</div>
+      <div>this plant is on growth stage: {plant.stage}</div>
+      <div>this plant last watered {plant.lastWatered.toISOString()}</div>
+    </div>
+  );
+};
 
 export default Plant;
