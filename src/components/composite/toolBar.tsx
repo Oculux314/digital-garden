722ba38e21446c41d1ef9549774a1befdd0355e9
zlrkw11 <ray040406@gmail.com>
2024-09-01 10:33:15 +1200

    context water state changer

--- src/components/composite/toolBar.tsx
+++ src/components/composite/toolBar.tsx
@@ -4,15 +4,17 @@ import Tool from "../tool";
 import shovelicon from "@/img/toolicons/shovel.png";
 import wateringcanicon from "@/img/toolicons/wateringcan.png";
 
-function water() {
-  console.log("");
-}
-
 export default function ToolBar() {
   const context = useAppContext();
   function shovel() {
     context.selectTool("shovel");
   }
+
+  function water() {
+    context.selectTool("water");
+    console.log("watered");
+  }
+
   return (
     // <div className="float-right flex gap-4 p-4 rounded-lg bg-cyan-200 mt-5 w-48">
     <div className="float-right flex flex-col gap-4 p-4 rounded-lg bg-cyan-200 mt-6 mr-5 w-24">
