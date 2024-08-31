6adefd0ebc1e601844ddc849697072fd83488662
jianing <lijn2003@protonmail.com>
2024-08-31 23:56:24 +1200

    Water plant route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,8 +1,13 @@
 "use server";
 import { UserType } from "@/models/user";
-import { stealPlant as _stealPlant } from "@/services/userService";
+import { stealPlant as _stealPlant, waterPlant as _waterPlant} from "@/services/userService";
 
 export async function stealPlant(
     user: UserType, otherUser: UserType, index: number) {
   _stealPlant(user, otherUser, index);
+}
+
+export async function waterPlant(
+    user: UserType, index: number) {
+  _waterPlant(user, index);
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,6 @@
 import { User, UserType } from "@/models/user";
-import { Plant, PlantType } from "@/models/plant";
+import { PlantType } from "@/models/plant";
+import { waterPlant as _waterPlant } from "./plantService";
 
 const MAX_PLANTS: number = 9;
 const TIME_FROM_LAST_WATERED: number = 24*60*60*1000; // 24 hours
@@ -10,10 +11,6 @@ export async function createUser(user: Omit<UserType, "id">) {
 
 export async function getUsers() {
   return User.find();
-}
-
-export async function getUserById(id: string): Promise<UserType | null> {
-  return User.findById(id);
 }
 
 export async function addPlant(user: UserType, plant: PlantType) {
@@ -30,18 +27,27 @@ export async function deletePlant(user: UserType, plant: PlantType) {
 
 export async function stealPlant(
     user: UserType, otherUser: UserType, index: number) {
-
-  if (index >= otherUser.plants.length) {
+      
+  if (user.plants.length >= MAX_PLANTS) {
+    throw Error("You already have " + MAX_PLANTS + " plants");
+  }
+  if (index >= user.plants.length) {
     throw Error("Plant does not exist");
   }
   let plant: PlantType = otherUser.plants[index];
-  if (user.plants.length >= MAX_PLANTS) {
-    throw Error("You already have " + MAX_PLANTS + " plants");
-  };
   if (Date.now() - plant.lastWatered.getTime()
       < TIME_FROM_LAST_WATERED) {
     throw Error("The plant is well-watered");
-  };
+  }
   deletePlant(otherUser, plant);
   addPlant(user, plant);
+}
+
+export async function waterPlant(
+    user: UserType, index: number) {
+
+  if (index >= user.plants.length) {
+    throw Error("Plant does not exist");
+  }
+  _waterPlant(user.plants[index]);
 }
