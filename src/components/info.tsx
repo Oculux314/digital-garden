251fb18fed00eb62bb81262101f9de6a1bb5b1d5
zlrkw11 <ray040406@gmail.com>
2024-08-31 14:07:40 +1200

    basic styling

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,7 +1,7 @@
 export default function InfoComponent() {
   return (
-    <div className="border border-black w-full h-16 rounded-lg p-8 flex justify-items-center">
-      <p>Info component</p>
+    <div className="border border-black w-[40%] rounded-lg p-8 col justify-items-center">
+      <h1 className="mb-4 font-bold text-red-500">Info component</h1>
       <p>placeholder text for this plant</p>
     </div>
   );
