bc79f505826161062ba10ace7f6360f0f18d8f42
Gavin Ding <gavinzlding@gmail.com>
2024-08-31 16:25:06 +1200

    x

--- src/app/test/example/page.tsx
+++ src/app/test/example/page.tsx
@@ -1,6 +1,5 @@
 // Go to /test/example to see the result
 
-import SlidingSidebar from "@/components/composite components/friendsBar";
 import InfoComponent from "@/components/info";
 
 export default function ExampleTest() {
---
+++ src/app/test/testCase1/plantCardTest.tsx
@@ -0,0 +1,5 @@
+import PlantCard from "@/components/plantCard";
+
+export default function ExampleTest() {
+  return <PlantCard />;
+}
