84ffa922a1c73f2c2d67317a937702986f923149
zlrkw11 <ray040406@gmail.com>
2024-08-31 20:31:22 +1200

    adjustments

--- src/components/plant.tsx
+++ src/components/plant.tsx
@@ -6,7 +6,7 @@ export type PlantProps = {
 
 const Plant = ({ plant }: PlantProps) => {
   return (
-    <div className="">
+    <div className="fixed mt-4 ml-4 bg-white">
       <div className="border border-black">insert plant image here</div>
       <div>this plant is on growth stage: {plant.stage}</div>
       <div>this plant last watered {plant.lastWatered.toISOString()}</div>
