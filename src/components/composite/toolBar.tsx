b2019d46e6fba43ed7ee6bdfa4c151dd7954405a
ZL <ray040406@gmail.com>
2024-09-01 02:10:51 +1200

    shovel grab fn

--- src/components/composite/toolBar.tsx
+++ src/components/composite/toolBar.tsx
@@ -2,13 +2,19 @@
 import Tool from "../tool";
 import { useState } from "react";
 
-function shovel() {}
+interface ToolBarProps {
+  onSelect: (tool: string) => void;
+}
 
 function water() {
   console.log("black men kissing andy xu baba");
 }
 
-export default function ToolBar() {
+export default function ToolBar({ onSelect }: ToolBarProps) {
+  const [grab, setGrab] = useState(false);
+  function shovel() {
+    setGrab(true);
+  }
   return (
     <div className="top-4 left-20 flex gap-4 p-4 border border-black rounded-lg bg-yellow-100 mt-5">
       <Tool image="/k.png" name="Shovel" onClick={shovel} />
