3f72cb90f6fded80ecdc4c01aceae4f112913cb0
zlrkw11 <ray040406@gmail.com>
2024-08-31 17:00:49 +1200

    imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import ToolBar from "@/components/composite components/toolBar";
+import InfoComponent from "@/components/info";
+import SearchBar from "@/components/searchBar";
+import FriendsBar from "@/components/composite components/friendsBar";
+
 export default function Home() {
   return <p>Home Page</p>;
 }
