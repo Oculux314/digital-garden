b93e316ea42d30e792372630c684d6388a5d7f8c
Gavin Ding <gavinzlding@gmail.com>
2024-08-31 15:34:56 +1200

    Friends bar component base finished; need backend and font/colour fixes etc

---
+++ src/app/test/testCase1/page.tsx
@@ -0,0 +1,5 @@
+import FriendsBar from "@/components/composite components/friendsBar";
+import SlidingSidebar from "@/components/composite components/friendsBar";
+export default function ExampleTest() {
+  return <FriendsBar />;
+}
