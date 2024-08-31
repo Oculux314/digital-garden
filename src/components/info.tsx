c5639f4eb189235ccd47a31cb326cf5bb0e30ac6
zlrkw11 <ray040406@gmail.com>
2024-08-31 19:43:43 +1200

    padding added

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -7,7 +7,7 @@ export default function InfoComponent() {
   return (
     <div className="flex absolute">
       {/* Plant Image Component */}
-      <div className="relative border border-black w-[400px] h-[500px] rounded-lg p-8 col justify-items-center">
+      <div className="relative border border-black w-[100px] h-[120px] rounded-lg p-8 col justify-items-center">
         <Image
           src={image}
           alt=""
@@ -18,7 +18,7 @@ export default function InfoComponent() {
       </div>
 
       {/* Info Component */}
-      <div className="border border-black w-[400px] h-[500px] rounded-lg p-8 col justify-items-center">
+      <div className="border border-black w-[200px] h-[120px] pl-4 pt-4 rounded-lg col justify-center">
         <h1 className="mb-4 font-bold text-red-500">Flower Name</h1>
         <p>Placeholder text for this plant</p>
       </div>
