64fccbb11761a787010b8ad3703af94c94abb9a6
Andy <andyxu024@gmail.com>
2024-09-01 11:36:38 +1200

    changed toolbar

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -16,7 +16,7 @@ const Tool = ({ image, name, onClick }: ToolProps) => {
   return (
     <button
       onClick={handleClick}
-      className="w-16 h-16 flex items-center justify-center border-none rounded-lg bg-teal-600 hover:bg-red-800"
+      className="w-16 h-16 flex items-center justify-center border-none rounded-lg bg-green-400 hover:bg-green-500"
     >
       <Image src={image} alt={name} width={58} height={58} />
     </button>
