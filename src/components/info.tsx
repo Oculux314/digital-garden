301edc42f2104d51293419d6caee3b13da991a17
zlrkw11 <ray040406@gmail.com>
2024-08-31 19:44:45 +1200

    daddy xu >_<

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useState } from "react";
 // import image from "@/img/placeholderimg.jpg";
-import image from "@/img/placeholderimg.jpg";
+import image from "@/img/k.png";
 
 export default function InfoComponent() {
   return (
@@ -18,7 +18,7 @@ export default function InfoComponent() {
       </div>
 
       {/* Info Component */}
-      <div className="border border-black w-[200px] h-[120px] pl-4 pt-4 rounded-lg col justify-center">
+      <div className="border bg-white border-black w-[200px] h-[120px] pl-4 pt-4 rounded-lg col justify-center">
         <h1 className="mb-4 font-bold text-red-500">Flower Name</h1>
         <p>Placeholder text for this plant</p>
       </div>
