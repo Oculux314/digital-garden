81498a847ecbe4c4b944494431b144a2b71699d8
jianing <lijn2003@protonmail.com>
2024-08-31 15:01:53 +1200

    Handle click

--- src/components/composite components/toolBar.tsx
+++ src/components/composite components/toolBar.tsx
@@ -16,9 +16,9 @@ function water() {
 export default function ToolBar() {
   return (
     <div className="absolute top-4 left-20 flex gap-4 p-4 border-4 border-black rounded-lg bg-yellow-100 mt-5">
-      <Tool src="shovel.png" alt="Shovel" f={shovel}/>
-      <Tool src="water.png" alt="Water" f={water}/>
-      {/* <Tool src="fertilizer.png" alt="Fertilizer" f={fertilizer}/> */}
+      <Tool src="shovel.png" name="Shovel" f={shovel}/>
+      <Tool src="water.png" name="Water" f={water}/>
+      {/* <Tool src="fertilizer.png" name="Fertilizer" f={fertilizer}/> */}
     </div>
   );
 }
--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -3,14 +3,19 @@ import React, { FC } from 'react';
 
 type ToolProps = {
   src: string;
-  alt: string;
+  name: string;
   f: () => void;
 }
 
-const Tool: FC<ToolProps> = ({src, alt, f}) => {
+const Tool: FC<ToolProps> = ({src, name, f}) => {
+  function handleClick() {
+    alert(name);
+    f();
+  }
+
   return (
-    <button onClick={f} className="w-16 h-16 flex items-center justify-center border-none rounded-lg bg-teal-600 hover:bg-teal-800">
-      <img src={src} alt={alt}/>
+    <button onClick={handleClick} className="w-16 h-16 flex items-center justify-center border-none rounded-lg bg-teal-600 hover:bg-teal-800">
+      <img src={src} alt={name}/>
     </button>
   );
 }
