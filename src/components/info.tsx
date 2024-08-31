bb27b107d6c2248341962f630225815cfadf73a3
zlrkw11 <ray040406@gmail.com>
2024-08-31 14:12:10 +1200

    image box

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -2,6 +2,7 @@ export default function InfoComponent() {
   return (
     <div className="border border-black w-[40%] rounded-lg p-8 col justify-items-center">
       <h1 className="mb-4 font-bold text-red-500">Info component</h1>
+      <img className="mb-2" alt="image placeholder" />
       <p>placeholder text for this plant</p>
     </div>
   );
