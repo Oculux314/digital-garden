2cce1937671a62eb5a62f127dac46d1875209793
Andy <andyxu024@gmail.com>
2024-09-01 05:41:18 +1200

    made header and added search bar to it

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from 'react';
+import Image from 'next/image';
+import mrflowerpoticon from "@/img/logos/mrflowerpoticonbig.png";
+import digitalgardenlogo from "@/img/logos/digitalgardenlogo.png";
+import SearchBar from './searchBar';
+
+export default function Header() {
+  return (
+    <header className="bg-cyan-300 p-6 flex items-center">
+      <div className="flex w-full items-center gap-4 mr-3">
+        {/* Logo */}
+        <Image src={mrflowerpoticon} alt="Logo" className="h-50 w-50" height={50} width={50} />
+        {/* Icon */}
+        <Image src={digitalgardenlogo} alt="Icon" className="h-100 w-300 ml-50" height={100} width={500} />
+        <div className="grow text-right">
+            <SearchBar/> 
+        </div>
+      </div>
+    </header>
+  );
+}
