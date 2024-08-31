f3d7093f82c80861760719d7e03d3a1513959b0b
zlrkw11 <ray040406@gmail.com>
2024-08-31 14:03:29 +1200

    structure

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,3 +1,8 @@
 export default function InfoComponent() {
-  return <div className="">Info component</div>;
+  return (
+    <div className="border border-black w-full h-16 rounded-lg p-8 flex justify-items-center">
+      <p>Info component</p>
+      <p>placeholder text for this plant</p>
+    </div>
+  );
 }
